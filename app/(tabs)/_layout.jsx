98e5b597b066fc73458ca21d9ad0b37eee216a15
yarwen0 <nhoojahm@gmail.com>
2025-02-12 23:38:41 -0600

    added tab navigation

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,37 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
     return (
-        <Tabs>
-            <Tabs.Screen name="home" />
-            <Tabs.Screen name="explore" />
-            <Tabs.Screen name="progress" />
-            <Tabs.Screen name="profile" />
+        <Tabs screenOptions={{
+            headerShown: false
+        }}>
+            <Tabs.Screen name="home" 
+            options={{
+                tabBarIcon:({color, size}) => <Ionicons name="home-outline" size={size} color={color} />,
+                tabBarLabel: 'Home'
+            }}
+            />
+            <Tabs.Screen name="explore" 
+            options={{
+                tabBarIcon:({color, size}) => <Ionicons name="search-outline" size={size} color={color} />,
+                tabBarLabel: 'Explore'
+            }}
+            />
+            <Tabs.Screen name="progress" 
+            options={{
+                tabBarIcon:({color, size}) => <Ionicons name="analytics-outline" size={size} color={color} />,
+                tabBarLabel: 'Progress'
+            }}
+            />
+            <Tabs.Screen name="profile" 
+            options={{
+                tabBarIcon:({color, size}) => <Ionicons name="person-circle-outline" size={size} color={color} />,
+                tabBarLabel: 'Profile'
+            }}
+            />
         </Tabs>
     )
 }
--- app/auth/signIn.jsx
+++ app/auth/signIn.jsx
@@ -24,6 +24,7 @@ export default function SignUp() {
                 console.log(user);
                 await getUserDetail();
                 setLoading(false);
+                router.replace('/(tabs)/home')
             })
             .catch((e) => {
                 console.log(e);
