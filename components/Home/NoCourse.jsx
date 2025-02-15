77c0a435e97b2f98f99da540b12a7f8302c1c210
yarwen0 <nhoojahm@gmail.com>
2025-02-15 00:00:00 -0600

    Added AddCourse screen

--- components/Home/NoCourse.jsx
+++ components/Home/NoCourse.jsx
@@ -1,8 +1,10 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import Button from '../Shared/Button'
+import { useRouter } from 'expo-router'
 
 export default function NoCourse() {
+  const router = useRouter();
   return (
     <View style={{
         marginTop: 40,
@@ -21,7 +23,7 @@ export default function NoCourse() {
             textAlign: 'center'
         }}>You Don't Have Any Course</Text>
 
-        <Button text = {'+ Create New Course'} />
+        <Button text = {'+ Create New Course'} onPress={() => router.push('/addCourse')}/>
         <Button text={'Explore Existing Courses'} 
           type='outline'
         />
