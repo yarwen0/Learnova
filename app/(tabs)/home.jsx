a7840deb11a36bc420202cc6e7309e22061d82a7
yarwen0 <nhoojahm@gmail.com>
2025-02-27 23:59:46 -0600

    Implement fetching and displaying course list for the user in Home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,32 @@
 import { View, Text, Platform } from "react-native";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "../../components/Home/Header";
 import Colors from './../../constant/Colors'
 import NoCourse from "../../components/Home/NoCourse";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../config/firebaseConfig";
+import { UserDetailContext } from "../../context/UserDetailContext";
 
 export default function Home() {
+
+    const {userDetail, setUserDetail} = useContext(UserDetailContext);
+    const [courseList, setCourseList] = useState([]);
+
+    useEffect(() => {
+        userDetail&&GetCourseList();
+    }, [userDetail])
+
+    const GetCourseList= async () => {
+        const q = query(collection(db, 'Courses'), where("createdBy",'==', userDetail?.email))
+        const querySnapshot = await getDocs(q);
+
+        querySnapshot.forEach((doc) => {
+            console.log("--", doc.data());
+        })
+
+    }
+
+
     return (
         <View style= {{
             padding: 25,
