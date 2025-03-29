f6992e7a8d3f109959650b213324c2f3706d6045
yarwen0 <nhoojahm@gmail.com>
2025-03-28 23:40:51 -0500

    Added Chapters to the home layout

--- components/Home/CourseList.jsx
+++ components/Home/CourseList.jsx
@@ -1,6 +1,8 @@
 import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
 import {imageAssets} from './../../constant/Option'
+import { StyleSheet } from 'react-native'
+import Colors from '../../constant/Colors'
 
 export default function CourseList({ courseList }) {
 
@@ -16,16 +18,24 @@ export default function CourseList({ courseList }) {
       <FlatList 
         data={courseList}
         horizontal={true}
+        showsHorizontalScrollIndicator={false}
         renderItem={({ item,index }) => (
           <View key={index} style={styles.courseContainer}>
-            <Image source={imageAssets[item.banner_image]} 
+              {/* <Image source={imageAssets[item.banner_image]}
               style={{
-                width: 260,
+                width: '100%',
                 height: 150,
                 borderRadius: 15
               }}
-            />
-            <Text>{item?.courseTitle}</Text>
+            /> */}
+            <Text style={{
+              fontFamily: 'outfit-bold',
+              fontSize: 18,
+              marginTop: 10
+            }}>{item?.courseTitle}</Text>
+            <Text style={{
+              fontFamily: 'outfit'
+            }}>{item?.chapters?.length} Chapters</Text>
           </View>
         )}
       />
@@ -35,7 +45,10 @@ export default function CourseList({ courseList }) {
 
 const styles = StyleSheet.create({
   courseContainer:{
-
-
+    padding: 10,
+    backgroundColor: Colors.BG_GRAY,
+    margin: 6,
+    borderRadius: 15,
+    width: 240
   }
 })
