83a62cd9868b5670eacf174534d9d33d6c32da61
yarwen0 <nhoojahm@gmail.com>
2025-04-06 22:46:28 -0500

    Added Practise section component

---
+++ components/Home/PractiseSection.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PractiseSection() {
+  return (
+    <View>
+      <Text>PractiseSection</Text>
+    </View>
+  )
+}
