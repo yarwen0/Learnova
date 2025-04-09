918a164fc37313fa082c0537b4b94f6a2ac060da
yarwen0 <nhoojahm@gmail.com>
2025-04-08 23:30:43 -0500

    Added Course Progress

---
+++ components/Home/CourseProgress.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CourseProgress() {
+  return (
+    <View>
+      <Text>CourseProgress</Text>
+    </View>
+  )
+}
