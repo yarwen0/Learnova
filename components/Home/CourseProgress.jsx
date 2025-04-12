5ed2134b00e39631b4a1c0ef129fed934bd0a986
yarwen0 <nhoojahm@gmail.com>
2025-04-11 23:58:07 -0500

    Added styling to the progress component

--- components/Home/CourseProgress.jsx
+++ components/Home/CourseProgress.jsx
@@ -1,6 +1,7 @@
 import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
 import { imageAssets } from '../../constant/Option'
+import Colors from '../../constant/Colors'
 
 export default function CourseProgress({courseList}) {
   return (
@@ -14,15 +15,43 @@ export default function CourseProgress({courseList}) {
 
       <FlatList
         data={courseList}
-        renderItem={({item, index})=>(
-          <View>
-              <View>
+        horizontal={true}
+        renderItem={({ item, index })=>(
+          <View style={{
+            margin: 7,
+            padding: 15,
+            backgroundColor: Colors.BG_GRAY,
+            borderRadius: 15,
+            width: 280
+          }}>
+              <View style={{
+                display: 'flex',
+                flexDirection: 'row',
+                gap: 8
+
+              }}>
                 <Image source={imageAssets[item?.banner_image]}
-                  style={{
-                    width: 60,
-                    height: 60
+                    style={{
+                        width: 80,
+                        height: 80,
+                        borderRadius: 8
                   }}
                 />
+                <View style={{
+                  flex: 1
+                }}>
+                    <Text 
+                    numberOfLines={2}
+                    style={{
+                      fontFamily: 'outfit-bold',
+                      fontSize: 19,
+                      flexWrap: 'wrap'
+                    }}>{item?.courseTitle}</Text>
+                    <Text style={{
+                      fontFamily: 'outfit',
+                      fontSize: 15
+                    }}>{item?.chapters?.length} Chapter</Text>
+                </View>
               </View>
           </View>
         )}
