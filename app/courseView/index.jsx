9ff0bb11f4b0d94c2f82e6a55c6bb9a4648fe56d
yarwen0 <nhoojahm@gmail.com>
2025-04-13 11:56:52 -0500

    Added images, title, icons to the component with some styling

--- app/courseView/index.jsx
+++ app/courseView/index.jsx
@@ -2,18 +2,16 @@ import { View, Text, Image } from 'react-native';
 import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
 import { imageAssets } from '../../constant/Option';
+import Intro from '../../components/CourseView/Intro';
 
 export default function CourseView() {
+
   const { courseParams } = useLocalSearchParams();
-  const course = JSON.parse(courseParams)
+  const course = JSON.parse(courseParams);
 
   return(
     <View>
-        <Image source={imageAssets[course?.banner_image]}
-                    style={{
-                        width: '100%'
-                    }}
-                />
-      </View>
+        <Intro course={course} />
+    </View>
   )
 }
--- components/CourseView/intro.jsx
+++ components/CourseView/intro.jsx
@@ -1,15 +1,40 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { imageAssets } from '../../constant/Option'
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-export default function intro({course}) {
+
+export default function Intro({course}) {
   return (
     <View>
       <Image source={imageAssets[course?.banner_image]}
                   style={{
-                      width: '100%'
+                      width: '100%',
+                      height: 280
                   }}
               />
+              <View style={{
+                padding: 20
+              }}>
+                <Text style={{
+                    fontFamily: 'outfit-bold',
+                    fontSize: 25
+                }}>{course?.courseTitle}</Text>
+                <View style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: 5,
+                    alignItems: 'center',
+                    marginTop: 5
+                }}>
+                    <Ionicons name="book-outline" size={20} color="black" />
+                    <Text style={{
+                        fontFamily: 'outfit',
+                        fontSize: 18
+                    }}>
+                        {course?.chapters?.length} Chapters</Text>
+                </View>
+              </View>
     </View>
   )
 }
