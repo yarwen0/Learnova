85eb566a54736cfe65bc40309249e236683cbd57
yarwen0 <nhoojahm@gmail.com>
2025-04-14 23:16:00 -0500

    Added ChapterView folder

---
+++ app/chapterView/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ChapterView() {
+  return (
+    <View>
+      <Text>ChapterView</Text>
+    </View>
+  )
+}
