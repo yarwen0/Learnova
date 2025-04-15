8b531dd72a1078c5a98ece449a072ac2dd2526aa
yarwen0 <nhoojahm@gmail.com>
2025-04-14 19:37:14 -0500

    Added chapters and some css

--- components/CourseView/Chapters.jsx
+++ components/CourseView/Chapters.jsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { StyleSheet } from 'react-native'
 
 export default function Chapters({course}) {
   return (
-    <View>
-      <Text>Chapters</Text>
+    <View style={{
+        padding: 20
+    }}>
+      <Text style={{
+        fontFamily: 'outfit-bold',
+        fontSize: 25
+      }}>Chapters</Text>
+
+      <FlatList 
+        data={course?.chapters}
+        renderItem={({item, index}) => (
+            <View style={{
+                padding: 18,
+                borderWidth: 0.5,
+                borderRadius: 15,
+                marginTop: 10
+            }}>
+                <View style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    gap: 10
+                }}>
+                    <Text style={styles.chapterText}>{index + 1}.</Text>
+                    <Text style={styles.chapterText}>{item?.chapterName}</Text>
+                </View>
+            </View>
+        )}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    chapterText:{
+        fontFamily: 'outfit',
+        fontSize: 20
+    }
+})
