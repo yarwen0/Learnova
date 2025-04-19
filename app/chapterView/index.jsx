bb4b13a6065bdd3eb6d039c5af4eb69a0e66ccd9
yarwen0 <nhoojahm@gmail.com>
2025-04-18 21:36:03 -0500

    Added Next and Finish button

--- app/chapterView/index.jsx
+++ app/chapterView/index.jsx
@@ -1,8 +1,9 @@
-import { View, Text, Dimensions, Platform } from 'react-native'
+import { View, Text, Dimensions, Platform, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
 import * as Progress from 'react-native-progress';
 import Colors from '../../constant/Colors';
+import Button from '../../components/Shared/Button';
 
 
 export default function ChapterView() {
@@ -13,6 +14,10 @@ export default function ChapterView() {
     const GetProgress=(currentPage)=> {
         const perc=(currentPage/chapters?.content?.length);
         return perc;
+    }
+
+    const onChapterComplete() ={
+
     }
   return (
     <View style={{
@@ -38,7 +43,34 @@ export default function ChapterView() {
                 fontSize: 20,
                 marginTop: 7
             }}>{chapters?.content[currentPage]?.explain}</Text>
+
+            {chapters?.content[currentPage]?.code && <Text style={[styles.codeExampleText, {backgroundColor: Colors.BLACK}]}>{chapters?.content[currentPage]?.code}</Text>}
+            {/* <Text>Example:</Text> */}
+            {chapters?.content[currentPage]?.example && <Text style={styles.codeExampleText}>{chapters?.content[currentPage]?.example}</Text>}
+
+        </View>
+
+        <View style={{
+            position: 'absolute',
+            bottom: 10,
+            width: '100%',
+            left: 25
+        }}>
+            {chapters?.content?.length-1 != currentPage?
+            <Button text={'Next'} onPress={() => setCurrentPage(currentPage+1)} />
+            : <Button text={'Finish'} onPress={() => onChapterComplete()}/>}
         </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    codeExampleText: {
+        padding: 15,
+        backgroundColor: Colors.BG_GRAY,
+        borderRadius: 15,
+        fontFamily: 'outfit',
+        fontSize: 18,
+        marginTop: 15
+    }
+})
--- constant/Colors.jsx
+++ constant/Colors.jsx
@@ -2,5 +2,6 @@ export default{
     WHITE:'#fff',
     PRIMARY:'#0075ff',
     GRAY: '#858585',
-    BG_GRAY: '#f2f2f2'
+    BG_GRAY: '#f2f2f2',
+    BLACK: '#000'
 }
