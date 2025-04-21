9ce61b40d4eb7116b4fe73a0b44809edb0af4009
yarwen0 <nhoojahm@gmail.com>
2025-04-20 23:52:24 -0500

    Added Practice Home screen and dynamic route

---
+++ app/practice/[type]/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PracticeTypeHomeScreen() {
+  return (
+    <View>
+      <Text>PracticeTypeHomeScreen</Text>
+    </View>
+  )
+}
