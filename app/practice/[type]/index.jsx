6fe5042ff13e278431dbf52a3a74b733ee62f9a6
yarwen0 <nhoojahm@gmail.com>
2025-04-21 02:08:39 -0500

    Now gets the data(quizzes) from firebase

--- app/practice/[type]/index.jsx
+++ app/practice/[type]/index.jsx
@@ -1,14 +1,35 @@
-import { View, Text, Image, SafeAreaView, Pressable } from 'react-native'
-import React from 'react'
+import { View, Text, Image, SafeAreaView, Pressable, ActivityIndicator } from 'react-native'
+import React, { useContext, useEffect, useState } from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import { imageAssets, PraticeOption } from '../../../constant/Option';
 import Colors from '../../../constant/Colors';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { query, collection, where, getDocs } from 'firebase/firestore';
+import {db} from './../../../config/firebaseConfig';
+import {UserDetailContext} from './../../../context/UserDetailContext';
 
 export default function PracticeTypeHomeScreen() {
     const router = useRouter();
     const {type} = useLocalSearchParams();
     const option = PraticeOption.find(item => item.name==type);
+    const {userDetail, setUserDetail} = useContext(UserDetailContext);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        userDetail&&GetCourseList();
+    }, [userDetail])
+
+    const GetCourseList = async() => {
+        setLoading(true);
+        const q = query(collection(db, 'Courses')
+        , where('createdBy', '==',userDetail?.email));
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+        })
+        setLoading(false);
+    }
   return (
     <SafeAreaView>
         <View>
@@ -37,6 +58,8 @@ export default function PracticeTypeHomeScreen() {
                     color: Colors.WHITE
                 }}>{type}</Text>
             </View>
+            {loading&&<ActivityIndicator size={'large'} 
+            color={Colors.PRIMARY} />}
         </View>
     </SafeAreaView>
 
