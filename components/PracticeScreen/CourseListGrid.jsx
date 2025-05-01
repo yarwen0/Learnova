da37dbde642a5e04a47ac4354c3cbcde69841847
yarwen0 <nhoojahm@gmail.com>
2025-04-30 21:25:43 -0500

    Done with CourseListGrid: Added Icons, Images, Styling, and Titles

--- components/PracticeScreen/CourseListGrid.jsx
+++ components/PracticeScreen/CourseListGrid.jsx
@@ -1,5 +1,7 @@
 import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
+import Colors from '../../constant/Colors'
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function CourseListGrid({ courseList, option }) {
   return (
@@ -14,15 +16,32 @@ export default function CourseListGrid({ courseList, option }) {
                 <View key={index} style={{
                     flex: 1,
                     display: 'flex',
-                    flexDirection: 'row',
+                    flexDirection: 'column',
                     alignItems: 'center',
-                    justifyContent: 'center'
+                    justifyContent: 'center',
+                    padding: 15,
+                    backgroundColor: Colors.WHITE,
+                    margin: 7,
+                    borderRadius: 15,
+                    elevation: 1
                 }}>
+                  <Ionicons name="checkmark-circle" size={24} color={Colors.GRAY} 
+                  style={{
+                    position: 'absolute',
+                    top: 10,
+                    right: 20
+                  }}
+                  />
                   <Image source={option?.icon} style={{
                     width: '100%',
-                    height: 100,
+                    height: 70,
                     objectFit: 'contain'
                   }}/>
+                  <Text style={{
+                    fontFamily: 'outfit',
+                    textAlign: 'center',
+                    marginTop: 7
+                  }}>{item.courseTitle}</Text>
                  </View>
             )} 
         />
