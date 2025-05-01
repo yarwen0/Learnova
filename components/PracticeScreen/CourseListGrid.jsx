1d4bb6b36a7adfae68ef2016721d8ce18a92277b
yarwen0 <nhoojahm@gmail.com>
2025-04-30 21:17:52 -0500

    Added Styling to the Practice Section

--- components/PracticeScreen/CourseListGrid.jsx
+++ components/PracticeScreen/CourseListGrid.jsx
@@ -6,12 +6,22 @@ export default function CourseListGrid({ courseList, option }) {
     <View>
         <FlatList 
             data={courseList}
-            
+            numColumns={2}
+            style={{
+                padding: 20
+            }}
             renderItem={({item, index})=>(
-                <View key={index}>
+                <View key={index} style={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
                   <Image source={option?.icon} style={{
-                    width: 100,
-                    height: 100
+                    width: '100%',
+                    height: 100,
+                    objectFit: 'contain'
                   }}/>
                  </View>
             )} 
