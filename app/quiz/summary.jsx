ad141d33761a8440e1daa6166e8997f0db6a5c5b
yarwen0 <nhoojahm@gmail.com>
2025-05-03 17:33:33 -0500

    Added summary.jsx to store quiz summaries

---
+++ app/quiz/summary.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function QuizSummary() {
+  return (
+    <View>
+      <Text>QuizSummary</Text>
+    </View>
+  )
+}
