b3050843ff339edf3f5a8781a2ea68d02a73b1ee
yarwen0 <nhoojahm@gmail.com>
2025-05-08 10:19:40 -0500

    Added a component to display the course by category

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -1,19 +1,35 @@
 import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 import Colors from './../../constant/Colors'
+import { CourseCategory } from './../../constant/Option'
+import CourseListByCategory from "../../components/Explore/CourseListByCategory";
 
 export default function Explore() {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{
+            backgroundColor: Colors.WHITE,
+            flex: 1}}>
             <View style={{
                 padding: 25,
                 backgroundColor: Colors.WHITE,
-                height: '100%'
+                flex: 1
                 }}>
                 <Text style={{
                     fontFamily: 'outfit-bold',
                     fontSize: 30
                 }}>Explore More Courses</Text>
+
+                {CourseCategory.map((item,index)=>(
+                    <View key={index} style={{
+                        marginTop: 10
+                    }}>
+                        <Text style={{
+                            fontFamily: 'outfit-bold',
+                            fontSize: 20
+                        }}>{item}</Text>
+                        <CourseListByCategory category={item} />
+                    </View>
+                ))}
             </View>
         </SafeAreaView>
     )
