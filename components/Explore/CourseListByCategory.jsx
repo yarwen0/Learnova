538eb7509ab64c96e781bd2cee2aece3bb88e938
yarwen0 <nhoojahm@gmail.com>
2025-05-08 10:33:52 -0500

    Got all the CourseList from firebase

--- components/Explore/CourseListByCategory.jsx
+++ components/Explore/CourseListByCategory.jsx
@@ -1,11 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  collection,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "./../../config/firebaseConfig";
 
-export default function CourseListByCategory({category}) {
+
+export default function CourseListByCategory({ category }) {
+
+    const [courseList, setCourseList] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        GetCourseListByCategory();
+    }, [category])
+
+  const GetCourseListByCategory = async () => {
+    setCourseList([]);
+    setLoading(true)
+    const q = query(
+      collection(db, "Courses"),
+      where("category", "==", category),
+      orderBy("createdOn", "desc")
+    );
+
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot?.forEach((doc) => {
+      console.log("--", doc.data());
+      setCourseList(prev => [...prev,doc.data()])
+    });
+    setLoading(false)
+  };
 
   return (
     <View>
       <Text>CourseListByCategory</Text>
     </View>
-  )
+  );
 }
